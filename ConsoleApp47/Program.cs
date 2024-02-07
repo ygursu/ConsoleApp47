38d2d0782f06de80470b5af28f16b90f921aace7
ygursu <yasemingrs@gmail.com>
2024-02-07 20:39:08 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp47/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int s1, s2, s3, buyuk, kucuk;
+
+Console.Write("Bir sayı girin");
+s1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Bir sayı girin");
+s2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Bir sayı girin");
+s3 = Convert.ToInt32(Console.ReadLine());
+
+
+if (s1 > s2 && s1 > s3)
+    buyuk = s1;
+else if (s2 > s3)
+    buyuk = s2;
+else
+    buyuk = s3;
+
+if (s1 < s2 && s1 < s3)
+    kucuk = s1;
+else if (s2 < s3)
+    kucuk = s2;
+else
+    kucuk = s3;
+
+
+Console.WriteLine("en büyük ile en küçük toplamı={0}", kucuk + buyuk);
+Console.ReadLine();
